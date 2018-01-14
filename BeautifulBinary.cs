01bff04ac2fb09fcf3b1b264196670b71b3743a9
Tanmay Ghosh <tnmyiitr@gmail.com>
2018-01-14 15:51:14 -0600

    Added Beautify Binary String code.

---
+++ BeautifulBinary.cs
@@ -0,0 +1,10 @@
+﻿namespace HackerRankCode
+{
+    class BeautifulBinary
+    {
+        public int beautifulBinaryString(string b)
+        {
+            return (b.Length - b.Replace("010", "").Length) / 3;
+        }
+    }
+}
